e690fc725a0457fdf937c2e57e998189a399f63c
rizayalcin <rzyalcin@gmail.com>
2024-12-16 18:28:29 +0300

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,14 @@
+// Database configuration
+const dbConfig = {
+    server: '185.155.149.114',
+    port: 49657,
+    database: 'TGDB',
+    user: 'sa',
+    password: 'Sax!24',
+    options: {
+        trustServerCertificate: true,
+        multipleActiveResultSets: false
+    }
+};
+
+export default dbConfig;
